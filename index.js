b707de5c60065d731752fd1578455ba645dd63ce
Gabriel Tartaglia <ga.tartaglia96@gmail.com>
2016-02-09 16:00:20 -0200

    Add mods feature

--- index.js
+++ index.js
@@ -15,11 +15,46 @@ class Dice {
 
     for (let i = 0; i < t; i++) {
       res.push(Math.floor(
-        this._random()
+        this._random() +
+        this.getModsSum()
       ));
     }
 
     return res.length === 1 ? res[0] : res;
+  }
+
+  addMod(mod) {
+    if (Array.isArray(mod)) {
+      mod.forEach((m) => this.addMod(m));
+      return this;
+    }
+
+    if (isNaN(mod)) {
+      throw new Error('Invalid mod type.');
+    }
+
+    this.mods.push(mod);
+    return this;
+  }
+
+  removeMod(mod) {
+    if (isNaN(mod)) {
+      throw new Error('Invalid mod type.');
+    }
+
+    this.mods.splice(this.mods.indexOf(mod), 1);
+    return this;
+  }
+
+  resetMods() {
+    this.mods = [];
+    return this;
+  }
+
+  getModsSum() {
+    let total = 0;
+    this.mods.forEach(mod => total += mod);
+    return total;
   }
 }
 
