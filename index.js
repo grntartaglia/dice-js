f85d1a0c7a8c90c957fb4625d665bc0ecea6c709
Gabriel Tartaglia <ga.tartaglia96@gmail.com>
2016-02-09 15:17:52 -0200

    Add Dice and roll method

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 'use strict';
+class Dice {
+  constructor(sides) {
+    this.sides = sides;
+    this.mods = [];
+  }
+
+  _random() {
+    return (Math.random() * this.sides) + 1;
+  }
+
+  roll(times) {
+    const t = times || 1;
+    let res;
+
+    if (t === 1) {
+      res = this._random();
+    } else {
+      res = [];
+
+      for (let i = 0; i < t; i++) {
+        res.push(this.roll());
+      }
+    }
+
+    return res;
+  }
+}
+
+module.exports = Dice;
